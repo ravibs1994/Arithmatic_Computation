d90cc9918c40954cd7e1ba00aa532ef5f8efeece
unknown <ravibs1994@gmail.com>
2020-10-10 13:41:03 +0530

    UC1_TakeInputs From User

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-
-read -p "Enter 1st input" $a
-read -p "Enter 2nd input" $b
-read -p "Enter 3rd input" $c
+read -p"Enter 1st input" a
+read -p"Enter 2nd input" b
+read -p"Enter 3rd input" c
 
